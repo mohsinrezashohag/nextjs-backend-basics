ad1576feabc7ae4695aaf30d7dce5f7ce4c930f1
mohsin reza shohag <mohsinrezashohag2@gmail.com>
2023-08-29 17:01:53 +0600

    next middleware concept

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -1,18 +1,22 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse, NextRequest } from 'next/server'
 import { redirect } from 'next/navigation'
 import { headers } from 'next/headers'
-
 
 export async function GET(req, res) {
     const id = await req.nextUrl.searchParams.get('id')
     // redirect
     // return redirect('/login')
-    return NextResponse.json({ id: id })
+    const headerList = headers()
+    return NextResponse.json({
+        id: 'got the response ',
+        user_name: headerList.get('user_name'),
+        user_id: headerList.get('user_id'),
+    })
 }
 export async function POST(req, res) {
     // handing for data
     const formData = await req.formData()
-    const image = formData.get("image")
+    const image = formData.get('image')
 
     // handing headers
     const headerList = headers()
@@ -20,12 +24,7 @@ export async function POST(req, res) {
 
     // work with cookies
     const myCookies = req.cookies.getAll()
-    console.log("myCookies :", myCookies);
-
-
-
+    console.log('myCookies :', myCookies)
 
     return NextResponse.json({ image: image, myToken: myHeader }, { status: 200 })
 }
-
-
--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,70 @@
-import { NextResponse, NextRequest } from "next/server";
+// Auth login
+// Cookies
+// Session
+// CSRF
+// Rate Limiting
+// Token Refresh
+// JWT Token Encode- Decode
+// private key
+
+import { headers } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+/*
+set headers from NextResponse
+*/
+
 
 export function middleware(req, res) {
-
-    // conditional middleware concept
     if (req.nextUrl.pathname.startsWith('/api/product')) {
-        console.log("middleware getting in product api");
+        const requestHeaders = new Headers(req.headers)
+        const token = requestHeaders.get('auth-token')
+
+
+        if (token === 'xyz-my-auth-23') {
+            const response = NextResponse.next()
+            response.headers.set('Set-Cookie', [
+                `name=mohsin reza; Path=/; Max-Age=3600; path=/; HttpOnly`,
+            ])
+            return response
+        }
+        else {
+            return NextResponse.json({
+                msg: "something wrong in middleware"
+            })
+        }
     }
 }
 
+
+
+
+
+
+/*
+set headers from NextRequest
+export function middleware(req, res) {
+    // conditional middleware concept
+    if (req.nextUrl.pathname.startsWith('/api/product')) {
+        const requestHeaders = new Headers(req.headers)
+        const token = requestHeaders.get('auth-token')
+
+        if (token === 'xyz-my-auth-23') {
+            // setting middleware through backend which is accessible on GET Request
+            requestHeaders.set('user_name', 'mohsin reza')
+            requestHeaders.set('user_id', '0009')
+            return NextResponse.next({
+                request: { headers: requestHeaders },
+            })
+        } else {
+            return NextResponse.json(
+                { message: 'something went wrong on middleware' },
+                { status: 401 }
+            )
+        }
+    }
+}
+*/
 export const config = {
     matcher: '/api/:path*',
 }
